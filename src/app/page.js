c1b28be58e7362fa6e8e37b86114141c72932fbb
mnacer <nmouzai@yahoo.ca>
2023-02-23 11:01:53 -0500

    fetch data from TMDB API

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-// import styles from './page.module.css'
+import Results from "@/components/Results";
 
-const inter = Inter({ subsets: ["latin"] });
+const API_KEY = process.env.API_KEY;
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
+  }
+  const data = await res.json();
+  const results = data.results;
 
-export default function Home() {
-  return <h1 className="text-red-500">Home</h1>;
+  return (
+    <h1 className="text-red-500">
+      <Results results={results} />
+    </h1>
+  );
 }
